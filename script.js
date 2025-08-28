46d0dfdabff042a3657b36ec25e6ae90bc561438
Sourov-Ahmed26 <silenboyk7@gmail.com>
2025-08-28 23:29:33 +0600

    Half Done

--- script.js
+++ script.js
@@ -9,22 +9,60 @@ for (const image of heartImages) {
         counterElement.innerText = count;
     });
 }
-
-
 // Copy Button Section 
-// Heart Counter Section
-const CopyBtns = document.querySelectorAll("#copy-btn");
-const counterCopy = document.getElementById('DisplayCopy');
 let CopyCount = 0;
+const counterCopy = document.getElementById("DisplayCopy"); 
 
-for (const btn of CopyBtns) {
-    btn.addEventListener('click', function () {
-        alert("Copied One")
+const copyButtons = document.querySelectorAll(".copy-btn");
+
+copyButtons.forEach(function (button) {
+    button.addEventListener("click", function () {
+        const card = button.closest(".card");
+
+        const numberEl = card.querySelector(".service-number");
+        const numberText = numberEl.textContent.trim();
+
+        const textarea = document.createElement("textarea");
+        textarea.value = numberText;
+        document.body.appendChild(textarea);
+        textarea.select();
+        document.execCommand("copy");
+        document.body.removeChild(textarea);
+        alert("Copied One");
         CopyCount++;
         counterCopy.innerText = CopyCount;
-    })
-}
+    });
+});
 
+const callButtons = document.querySelectorAll(".call-btn");
+const historyContainer = document.getElementById('callHistory');
+callButtons.forEach(function (button) {
+    button.addEventListener("click", function () {
+        const card = button.closest(".card");
+        const nameEl = card.querySelector(".service-name");
+        const numberEl = card.querySelector(".service-number");
+
+        const serviceName = nameEl.textContent.trim();
+        const numberText = numberEl.textContent.trim();
+        console.log(serviceName);
+
+        const now = new Date();
+        const timeString = now.toLocaleTimeString();
+
+        const historyItem = document.createElement("div");
+        historyItem.className = "flex justify-between items-center p-4 gap-4 border border-neutral-300 rounded-xl shadow-sm";
+        historyItem.innerHTML = `
+        <div>
+          <h1>${serviceName}</h1>
+          <p>${numberText}</p>
+        </div>
+        <div>
+          <h1>${timeString}</h1>
+        </div>
+      `;
+        historyContainer.append(historyItem);
+    });
+});
 // Call Btn Section
 
 // const coinCountElement = document.getElementById('coins');
